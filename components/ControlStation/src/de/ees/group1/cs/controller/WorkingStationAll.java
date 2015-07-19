4fdada89c508a6088bd058912ea8a6eaf7da2baa
Steich <s0660494@tu-dresden.de>
2015-07-19 18:20:28 +0200

    implementing method for setting the workingStation in an array

--- components/ControlStation/src/de/ees/group1/cs/controller/WorkingStationAll.java
+++ components/ControlStation/src/de/ees/group1/cs/controller/WorkingStationAll.java
@@ -1,8 +1,31 @@
 package de.ees.group1.cs.controller;
 
-import de.ees.group1.com.IWorkStation;
+
 import de.ees.group1.cs.gui.IWorkstationController;
+import de.ees.group1.cs.gui.MainWindow;
+import de.ees.group1.model.WorkstationType;
 
-public abstract class WorkingStationAll implements IWorkstationController, IWorkStation{
 
+public  class WorkingStationAll  implements IWorkstationController{
+	private MainWindow mainWindow;
+	private WorkingStation workStation[]=new WorkingStation[4];
+
+	
+	public void setWorkingStation(WorkingStation workStation){
+		int i=workStation.getId();
+		this.workStation[i]=workStation;
+	}
+	public void workstationTypeUpdatedAction(int id, WorkstationType type) {
+		workStation[id].setType(type);
+		mainWindow.updateWorkstationState();
+		
+	}
+
+	
+	public void workstationQualityUpdatedAction(int id, int quality) {
+		workStation[id].setMaxQualityLevel(quality);
+		mainWindow.updateWorkstationState();
+		
+	}
+	
 }
