a80d56f57af1d8df0f6293899f118673780f32c3
Steich <s0660494@tu-dresden.de>
2015-07-19 17:22:12 +0200

    init abstract class WorkingStationAll

---
+++ components/ControlStation/src/de/ees/group1/cs/controller/WorkingStationAll.java
@@ -0,0 +1,8 @@
+package de.ees.group1.cs.controller;
+
+import de.ees.group1.com.IWorkStation;
+import de.ees.group1.cs.gui.IWorkstationController;
+
+public abstract class WorkingStationAll implements IWorkstationController, IWorkStation{
+
+}
